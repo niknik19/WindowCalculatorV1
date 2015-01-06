73078fb59217b480cbcabbc865c8598182098b93
niknik19 <1nik-nik@mail.ru>
2015-01-07 02:20:58 +0300

    Added full functionality

---
+++ js/windows-calculator.js
@@ -0,0 +1,81 @@
+﻿$(function() {
+    $('.wc-type-definition-group-element').on('click', function (e) {
+        var $this = $(this);
+        if ($this.hasClass('wc-type-definition-group-element-selected'))
+            return;
+
+        $('.wc-window-image').attr('src', 'img/big/b' + $this.data('type') + '.jpg');
+
+        $('.wc-type-definition-group-element-selected').removeClass('wc-type-definition-group-element-selected');
+        $this.addClass('wc-type-definition-group-element-selected');
+
+        var carriage = $('.wc-selected-carriage');
+
+        carriage.css("left", $this.position().left);
+        carriage.width($this.width() + 9);
+        carriage.height($this.height() + 10);
+    });
+
+    var usdRate = 14000;
+
+    var productTypePrice = {
+        '1ct1': 756,
+        '1ct2': 0,
+        '1ct3': 1270,
+        '2ct1': 890,
+        '2ct2': 1130,
+        '3ct1': 770,
+        '3ct2': 940,
+        '3ct3': 1080,
+        '4ct1': 830,
+        '4ct2': 1010,
+        'db1': 1000,
+        'db2': 1100
+    };
+
+    var productTypePriceFactor = 10000000;
+
+    var producerRatio = {
+        DEXEN: 100,
+        WDS: 108,
+        KBE: 111,
+        SALAMANDER: 132
+    };
+
+    var producerRatioFactor = 100;
+
+    $('.wc-button-calculate').on('click', function(e) {
+        var height = + $('.wc-height-block-value').val();
+        var width = + $('.wc-width-block-value').val();
+        if (! (height > 0 && width > 0)) {
+            showError();
+            return;
+        }
+
+        var productType = $('.wc-type-definition-group-element-selected').data('type');
+        var producer = $('.wc-producer').val();
+
+        var price = Math.ceil(
+            ((height * width) * producerRatio[producer] * productTypePrice[productType] * usdRate) /
+            (producerRatioFactor * productTypePriceFactor));
+
+        if (price > 1000000000){
+            showError();
+            return;
+        } else {
+            showPrice(price);
+        }
+    });
+
+    function showError() {
+        $('.wc-money-price-fieldset').removeClass('wc-hidden');
+        $('.wc-money-price-rubles').addClass('wc-hidden');
+        $('.wc-money-price').text('Ошибка...');
+    }
+
+    function showPrice(price) {
+        $('.wc-money-price-fieldset').removeClass('wc-hidden');
+        $('.wc-money-price-rubles').removeClass('wc-hidden');
+        $('.wc-money-price').text(price.toString().split(/(?=(?:...)*$)/).join(' '));
+    }
+});
